6bb8367d165fcd7ca0393b48b1e6bab5c5807988
jimmievan98 <jimmievan98@gmail.com>
2026-03-19 15:45:03 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './ascend-league.jsx
+import App from './ascend-league.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
